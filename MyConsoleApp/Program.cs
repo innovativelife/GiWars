0008074b397d257a24ac445059d828bf28410d86
levismith <levi.js.bris@gmail.com>
2025-11-30 14:12:35 +1000

    Changed Program to be more accurate

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Levi!  You lose!!!!!!");
+Console.WriteLine("These were the last words Greg said. He died shortly after. Very sad :(")
